81f7e5a34cb323508d4e5ce90b909c7b4b2e0c8b
Rasel Hossain Adib <97701792+AdibRasel@users.noreply.github.com>
2023-10-18 01:23:41 +0600

    Class 3

---
+++ Class/class 2/Class2.dart
@@ -0,0 +1,58 @@
+void main() {
+  // ClassOne classOne = ClassOne();
+  // print(classOne.getAssignmentQ3());
+
+  // Map<String, int> ages = {
+  //   'Alina': 25,
+  //   'Mark': 30,
+  //   'Sophia': 28,
+  // };
+  // print(ages);
+
+  // Map<String, String> LikeObject = {"Name": "Rasel Hossain", "age": "23"};
+
+  // Map<String, int> SchlInfo = {"Name": 123, "age": 569};
+//
+
+  // Map<dynamic, dynamic> StudentInfo = {
+  // "SchoolName": "Shahapur M.L High School",
+  // "RoolNo": 133,
+  // "isStudent": true,
+  // };
+  // Mapp এর মধ্যে আরো ডাটা যোগ করা
+  // StudentInfo["class"] = "Nine";
+
+  // Mapp এর মধ্যে ডাটা ডিলিট করা
+  // StudentInfo.remove("RoolNo");
+
+  // Mapp এর মধ্যে সকল কি(key) দেখা
+  // print(StudentInfo.keys);
+
+  // Mapp এর মধ্যে সকল ভ্যালু(value) দেখা
+  // print(StudentInfo.values);
+
+  // Mapp এর ভ্যালু(value) আপডেট করা
+  // StudentInfo.update("SchoolName", (value) => "Sompara School");
+
+  // Mapp এর মধ্যে লুপ চালানো
+  // StudentInfo.forEach((key, value) {
+  //   print(key);
+  //   print(value);
+  // });
+
+  // Map<String, dynamic> MyData = {
+  //   'name': 'Country',
+  //   'data': <String, String>{
+  //     'name': 'Bangladesh',
+  //     'polulation': '20 Core',
+  //     'prime-minister': 'Shekh Hasina'
+  //   }
+  // };
+  // // Q1 ans:-
+  // dynamic shekhHasina = MyData['data']['prime-minister'];
+  // // Q2 ans:-
+  // MyData['data'].remove('name');
+  // // Q3 ans:-
+  // MyData['data'].update("polulation", (value) => "New Value");
+  // print(MyData);
+}
--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,60 +1,37 @@
-import "../Class/Class1/Class1.dart";
-
 void main() {
-  // ClassOne classOne = ClassOne();
-  // print(classOne.getAssignmentQ3());
+  // int a = 100;
+  // int b = 200;
 
-  // Map<String, int> ages = {
-  //   'Alina': 25,
-  //   'Mark': 30,
-  //   'Sophia': 28,
-  // };
-  // print(ages);
+  // var Login = true;
+  // var IsAdmin = true;
 
-  // Map<String, String> LikeObject = {"Name": "Rasel Hossain", "age": "23"};
+  // if (Login == true) {
+  //   print("User Already Logged In");
+  // } else if (IsAdmin == false) {
+  //   print("He is not an Admin");
+  // } else {
+  //   print("Unauthorized");
+  // }
 
-  // Map<String, int> SchlInfo = {"Name": 123, "age": 569};
-//
 
-  // Map<dynamic, dynamic> StudentInfo = {
-  // "SchoolName": "Shahapur M.L High School",
-  // "RoolNo": 133,
-  // "isStudent": true,
-  // };
-  // Mapp এর মধ্যে আরো ডাটা যোগ করা
-  // StudentInfo["class"] = "Nine";
 
-  // Mapp এর মধ্যে ডাটা ডিলিট করা
-  // StudentInfo.remove("RoolNo");
 
-  // Mapp এর মধ্যে সকল কি(key) দেখা
-  // print(StudentInfo.keys);
+  for (int i = 0; i < 100; i++) {
 
-  // Mapp এর মধ্যে সকল ভ্যালু(value) দেখা
-  // print(StudentInfo.values);
 
-  // Mapp এর ভ্যালু(value) আপডেট করা
-  // StudentInfo.update("SchoolName", (value) => "Sompara School");
+    print(i);
 
-  // Mapp এর মধ্যে লুপ চালানো
-  // StudentInfo.forEach((key, value) {
-  //   print(key);
-  //   print(value);
-  // });
 
-  Map<String, dynamic> MyData = {
-    'name': 'Country',
-    'data': <String, String>{
-      'name': 'Bangladesh',
-      'polulation': '20 Core',
-      'prime-minister': 'Shekh Hasina'
-    }
-  };
-  // Q1 ans:-
-  dynamic shekhHasina = MyData['data']['prime-minister'];
-  // Q2 ans:-
-  MyData['data'].remove('name');
-  // Q3 ans:-
-  MyData['data'].update("polulation", (value) => "New Value");
-  print(MyData);
+  }
+  
+
+  num Condition = 100;
+  for (int InitialValue = 0; InitialValue < Condition; InitialValue++) {
+
+
+    print(InitialValue);
+
+  }
+
+
 }
